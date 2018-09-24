a1a6ff679a24bdc89751a25bab3dd3f77971eba5
Manny Martinez <vccengineers12@gmail.com>
2018-09-23 21:27:54 -0700

    Added followup for permutations to prevent duplications

--- recursion_review/permutation/index.js
+++ recursion_review/permutation/index.js
@@ -1,4 +1,4 @@
-// Print all the permutations of an array, recursively
+// Given an array, print all of the permutations of the items within it. Don't worry about duplications
 
 function permute(strArr, collection = []) {
     if (!strArr.length) {
@@ -18,3 +18,27 @@ function permute(strArr, collection = []) {
 }
 
 permute(['a', 'b', 'c', 'd']);
+
+// Follow up, how would you prevent the printing of duplicates?
+function permuteUnique(strArr, collection = [], printed = new Set()) {
+    if (!strArr.length) {
+        const collectionString = collection.toString();
+        if (!printed.has(collectionString)) {
+            printed.add(collectionString);
+            console.log(collectionString);
+        }
+    } else {
+        for (let i = 0; i < strArr.length; i++) {
+            // choose
+            collection.push(strArr.splice(i, 1));
+
+            // explore
+            permute(strArr, collection, printed);
+
+            // unchoose
+            strArr.splice(i, 0, collection.pop());
+        }
+    }
+}
+
+permuteUnique(['a', 'b', 'c', 'a', 'd']);
